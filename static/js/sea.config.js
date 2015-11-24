2098455fa9598f916ccd9d25697811e11ac4460c
MaChi <loyuk@sina.com>
2015-11-24 17:13:58 +0800

    重新规划JS组件的位置
    重新规划js组件的位置

--- static/js/sea.config.js
+++ static/js/sea.config.js
@@ -2,16 +2,16 @@ var version = '0.9.0';
 seajs.config({
     alias: {
         //init: 'init.js',
-        jquery: 'jquery.js',
+        jquery: 'js/jquery.js',
 
         //helloworld
-        marked: 'editor/marked.js', //markdown parser
-        form: 'editor/jquery.form.js', //jquery.form.js
-        HelloWorld: 'editor/components/helloworld.js',
-        TextArea: 'editor/components/textarea.js',
-        EditorCSS: 'editor/css/editor.css',
-        MarkdownParser: 'editor/components/markdownparser.js',
-        Util: 'editor/components/util.js'
+        marked: 'components/editor/marked.js', //markdown parser
+        form: 'components/editor/jquery.form.js', //jquery.form.js
+        HelloWorld: 'components/editor/components/helloworld.js',
+        TextArea: 'components/editor/components/textarea.js',
+        EditorCSS: 'components/editor/css/editor.css',
+        MarkdownParser: 'components/editor/components/markdownparser.js',
+        Util: 'components/editor/components/util.js'
         
     },
     map: [
@@ -21,5 +21,6 @@ seajs.config({
     debug: true,
     // preload: ['init'], // jquery所有版本适用
     preload: ['jquery'],// jquery1.10以上使用
-    debug: true
+    debug: true,
+    base: './static/'
 });
