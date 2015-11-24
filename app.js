5228f00fda26dd7a73e9e80b6c495ab7024eafc1
MaChi <loyuk@sina.com>
2015-11-24 17:39:07 +0800

    update
    update

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app/routes/index');
+var users = require('./app/routes/users');
 var session = require('express-session'); //支持session
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret:'myself'})); //session;
-app.use('/static', express.static('static')); //静态文件
+app.use('/static', express.static('public/static')); //静态文件
 
 
 app.use('/', routes);
