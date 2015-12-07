82ebf72172a7a61e812a590c7af6373c3b780050
MaChi <loyuk@sina.com>
2015-12-07 14:13:36 +0800

    dev
    增加DashBoard的左侧导航点击功能

---
+++ public/static/js/dashboardConfig.js
@@ -0,0 +1,30 @@
+/*
+ * 仪表盘配置文件
+ * @author machi
+ */
+define(function (require, exports, module){
+    var config = {
+        ctrlList: [
+            {
+                name: '写文章',
+                link: '/admin/editor/'
+            }, {
+                name: '文章管理',
+                link: ''
+            }, {
+                name: '分类',
+                link: ''
+            },  {
+                name: '评论',
+                link: ''
+            },  {
+                name: '图片',
+                link: ''
+            },  {
+                name: '设置',
+                link: ''
+            }
+        ]
+    }
+    module.exports = config;
+});
