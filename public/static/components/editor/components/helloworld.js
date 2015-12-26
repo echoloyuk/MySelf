7b35b16c414315c54f504483f1dc1e6936084b7d
MaChi <loyuk@sina.com>
2015-12-26 13:57:23 +0800

    Revert "Revert "Revert "Revert "dev""""
    This reverts commit 06d6526fe39470fd0bde46f68a092aeab31600fd.

--- public/static/components/editor/components/helloworld.js
+++ public/static/components/editor/components/helloworld.js
@@ -682,6 +682,20 @@ define(function (require, exports, module){
                 str += '不能临时保存，保存失败';
             }
             $info.html(str);
+        }, 
+
+        //获取title
+        getTitle: function (){
+            var $target = this.$target;
+            var $title = $('#hTitle', $target);
+            return $title.val();
+        }, 
+
+        //获取content
+        getContent: function (){
+            var $target = this.$target;
+            var $content = $('#hContent', $target);
+            return $content.val();
         }
     });
 
